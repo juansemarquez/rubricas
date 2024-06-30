6b2177fa9e4b7091f9fee4dabb551465d77bc6a0
jmarquez-urq <jmarquez.urquiza@gmail.com>
2024-06-30 10:51:37 -0300

    Utiliza CDN de bootstrap

--- escribir_archivos.php
+++ escribir_archivos.php
@@ -26,7 +26,7 @@ fclose($archivo);
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" href="bootstrap.min.css" type="text/css" /> 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <title>Archivos escritos</title>
     </head>
     <body class="container txt-center">
